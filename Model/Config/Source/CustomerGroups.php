7ad1fb078e046b20e2d097187ae1ffae253969a8
Robin Hunter <rhunter@crml.com>
2019-07-08 07:47:37 +0100

    Changed admin customer group to be dropdown

---
+++ Model/Config/Source/CustomerGroups.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Copyright © 2019 Roma Technology Limited. All rights reserved.
+* See COPYING.txt for license details.
+*/
+namespace RTech\Zoho\Model\Config\Source;
+
+class CustomerGroups implements \Magento\Framework\Option\ArrayInterface{
+  protected $_customerGroup;
+  protected $_categoryCollectionFactory;
+
+  public function __construct(
+    \Magento\Customer\Model\ResourceModel\Group\Collection $customerGroup
+  ) {
+    $this->_customerGroup = $customerGroup;
+  }
+
+  public function toOptionArray() {
+    return $this->_customerGroup->toOptionArray();
+  }
+}
